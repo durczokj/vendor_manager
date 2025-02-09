1f8b19d7883ffaa417100879e7f0ce62c8190974
Jakub Durczok <durczok.jakub@gmail.com>
2025-02-09 20:41:47 +0100

    flake8

--- companies/views.py
+++ companies/views.py
@@ -1,27 +1,34 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Company
-from django.shortcuts import render, get_object_or_404
+"""Views for the companies app."""
+
 from django.contrib.auth.decorators import login_required
-from rolepermissions.checkers import has_object_permission
 from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, render
+from rolepermissions.checkers import has_object_permission
+
+from .models import Company
+
 
 @login_required
 def companies(request):
+    """List all companies that the user has access to."""
     mycompanies = Company.objects.all()
-    mycompanies = [c for c in mycompanies if has_object_permission('access_company', request.user, c)]
-    return render(request, 'all_companies.html', {'mycompanies': mycompanies})
+    mycompanies = [
+        c
+        for c in mycompanies
+        if has_object_permission("access_company", request.user, c)
+    ]
+    return render(request, "all_companies.html", {"mycompanies": mycompanies})
+
 
 @login_required
-def company_details(request, id):
-    company = get_object_or_404(Company, id=id)
+def company_details(request, company_id):
+    """Show details of a company."""
+    company = get_object_or_404(Company, id=company_id)
 
-    if not has_object_permission('access_company', request.user, company):
+    if not has_object_permission("access_company", request.user, company):
         return HttpResponseForbidden()
 
     orders = company.orders.all()
     return render(
-        request,
-        'company_details.html', 
-        {'company': company,
-         'orders': orders})
+        request, "company_details.html", {"company": company, "orders": orders}
+    )
