936bbf20d936b5ca94270ac8e05e6d635cae7a48
Jakub Durczok <durczok.jakub@gmail.com>
2025-06-15 20:02:16 +0200

    add

---
+++ vendor_manager/deployment.py
@@ -0,0 +1,21 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ.get("WEBSITE_HOSTNAME")]
+CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('WEBSITE_HOSTNAME')}"]
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': "mssql",
+        'NAME': "vendor_manager",
+        'USER': "jakubdurczok",
+        'PASSWORD': 'Wynalazek3D!',
+        'HOST': 'serverjd107950.database.windows.net',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+    }
+}
+}
