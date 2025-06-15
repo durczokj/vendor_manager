59709d1a4415467a2fbf2fde1726b531c1aacd48
Jakub Durczok <durczok.jakub@gmail.com>
2025-06-15 20:47:15 +0200

    fix

--- vendor_manager/deployment.py
+++ vendor_manager/deployment.py
@@ -6,6 +6,8 @@ ALLOWED_HOSTS = [os.environ.get("WEBSITE_HOSTNAME")]
 CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('WEBSITE_HOSTNAME')}"]
 DEBUG = True
 
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
 DATABASES = {
     'default': {
         'ENGINE': "mssql",
